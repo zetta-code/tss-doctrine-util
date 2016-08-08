f113e2d3b6fbebe3d7d24fd0be33c415f6b69a5b
Thiago S. Santos <thiagos.xsantos@gmail.com>
2016-08-08 00:48:42 -0300

    Add Module and ConfigProvider

---
+++ src/ConfigProvider.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @link      http://github.com/zetta-repo/tss-doctrineutil for the canonical source repository
+ * @copyright Copyright (c) 2016 Zetta Code
+ */
+
+namespace TSS\Bootstrap;
+
+class ConfigProvider
+{
+    /**
+     * Return configuration for this component.
+     *
+     * @return array
+     */
+    public function __invoke()
+    {
+        return [];
+    }
+}
---
+++ src/Module.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @link      http://github.com/zetta-repo/tss-doctrineutil for the canonical source repository
+ * @copyright Copyright (c) 2016 Zetta Code
+ */
+
+namespace TSS\DoctrineUtil;
+
+class Module
+{
+    /**
+     * Provide application configuration.
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return [];
+    }
+}
